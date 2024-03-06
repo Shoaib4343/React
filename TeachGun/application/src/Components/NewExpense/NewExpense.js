6e54482fac3e6aa655b20cf4350e5742d37841ae
shoaib <mohammadshoaibkhan4343@gmail.com>
2024-03-06 13:40:44 +0500

    lifting state up or child-to-parent communication

--- TeachGun/application/src/Components/NewExpense/NewExpense.js
+++ TeachGun/application/src/Components/NewExpense/NewExpense.js
@@ -2,11 +2,21 @@ import React from "react";
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = ()=>{
+const NewExpense = (props)=>{
+
+    const getformData = (data) =>{
+        const newData = {
+            ...data,
+            id: Math.floor(Math.random() * 100000).toString().padStart(5, '0')
+        };
+        props.getExpenseData(newData);
+        console.log(newData);
+    };
+    
     return(
         <>
             <div className="new-expense">
-                <ExpenseForm />
+                <ExpenseForm getformData={getformData}  />
             </div>
             
         </>
